9b8458ba74c82d4585fc3bc9a7953d12955428c6
Amit gupta <billamitgupta@gmail.com>
2023-11-27 02:38:04 +0530

    first commit

---
+++ string_medthod.js
@@ -0,0 +1,15 @@
+const getName = new String("hello amit gupta");
+const name = "Amit"
+console.log(getName.split(' '));
+ console.log(`hello, myname is ${name}` ); 
+ const newDate = new Date()
+ console.log(Math.floor((newDate.getTime())/100000 )) 
+ console.log(Math.sqrt(500))
+ newDate.toLocaleDateString()
+ const arr1 = ["amit","amit2","amit3"]
+ const arr2= ["amit4","amit5","amit6"]
+
+ const newArr = arr1.concat(arr2)
+ console.log(newArr)
+ const newArr2 = [...arr1,...arr2]
+ console.log(newArr2)
